0b8b2ed9ba889726f1ce8178c88b42213b8b10c6
yusufamr <50525245+yusufamr@users.noreply.github.com>
2021-09-06 10:40:53 +0200

    Delete BinarySearch.h

--- BinarySearch.h
+++
@@ -1,8 +0,0 @@
-#ifndef BINARYSEARCH_H_INCLUDED
-#define BINARYSEARCH_H_INCLUDED
-#include <stdio.h>
-#include <stdlib.h>
-#include "Sorting.h"
-sint8 binarySearch(sint8 *Arr,sint8 last,sint8 first,sint8 key);
-
-#endif // BINARYSEARCH_H_INCLUDED
